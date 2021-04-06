c169a63afbe1af5f4c6839984bd4a1fb176fd0c7
leod <subtle.frustration@proton.me>
2021-04-06 21:57:55 +0200

    Allow choosing other optimizers

--- tools/train_genre_classifier.py
+++ tools/train_genre_classifier.py
@@ -8,7 +8,7 @@ import argparse
 import git
 
 import torch
-from torch.optim import SGD
+from torch.optim import SGD, AdamW
 from torch.utils.data import DataLoader
 import torchvision
 
@@ -48,6 +48,16 @@ def load_data(data_dir, subset, num_workers, batch_size, input_size):
         'validation': val_loader
     }
 
+
+def create_optimizer(params_to_update,
+                     lr,
+                     momentum,
+                     optimizer):
+    if optimizer == 'SGD':
+        return SGD(params_to_update, lr=lr, momentum=momentum)
+    elif optimizer == 'AdamW':
+        return AdamW(params_to_update, lr=lr)
+
 def run(data_dir,
         subset,
         num_workers,
@@ -58,6 +68,7 @@ def run(data_dir,
         num_epochs,
         lr,
         momentum,
+        optimizer,
         device):
     logger.info(f'Model name: {model_name}')
     model, input_size = image_head.initialize_model(model_name,
@@ -71,7 +82,7 @@ def run(data_dir,
     num_trainable = sum(p.numel() for p in params_to_update)
     logger.info(f'Number of trainable parameters: {num_trainable}')
 
-    optimizer = SGD(params_to_update, lr=lr, momentum=momentum)
+    optimizer = create_optimizer(params_to_update, lr, momentum, optimizer)
 
     _, val_acc_history = classifier.train(model,
                                           dataloaders,
@@ -87,7 +98,8 @@ def run_all(data_dir,
             batch_size,
             num_epochs,
             lr,
-            momentum):
+            momentum,
+            optimizer):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     repo = git.Repo(os.path.dirname(sys.argv[0]), search_parent_directories=True)
@@ -101,7 +113,8 @@ def run_all(data_dir,
                 f'    batch_size={batch_size}\n'
                 f'    num_epochs={num_epochs}\n'
                 f'    lr={lr}\n'
-                f'    momentum={momentum}')
+                f'    momentum={momentum}\n'
+                f'    optimizer={optimizer}')
 
     model_top_val_accs = {}
 
@@ -109,16 +122,17 @@ def run_all(data_dir,
         top_val_accs = []
         for _ in range(num_runs):
             val_acc_history = run(data_dir=data_dir,
-                                subset=subset,
-                                num_workers=num_workers,
-                                model_name=model_name,
-                                train_encoder=True,
-                                use_pretrained=True,
-                                batch_size=batch_size,
-                                num_epochs=num_epochs,
-                                lr=lr,
-                                momentum=momentum,
-                                device=device)
+                                  subset=subset,
+                                  num_workers=num_workers,
+                                  model_name=model_name,
+                                  train_encoder=True,
+                                  use_pretrained=True,
+                                  batch_size=batch_size,
+                                  num_epochs=num_epochs,
+                                  lr=lr,
+                                  momentum=momentum,
+                                  optimizer=optimizer,
+                                  device=device)
             top_val_accs.append(max(val_acc_history))
 
         model_top_val_accs[model_name] = top_val_accs
@@ -161,6 +175,10 @@ if __name__ == '__main__':
                         help='Momentum for the SGD optimizer',
                         type=float,
                         default=0.9)
+    parser.add_argument('--optimizer',
+                        help='Optimizer to use',
+                        choices=['SGD', 'AdamW'],
+                        default='SGD')
 
     args = parser.parse_args()
     run_all(**vars(args)) 
